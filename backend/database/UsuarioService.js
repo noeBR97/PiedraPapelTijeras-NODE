e042e67d8b1d7e5a2719b941032bfad05b194f3a
Noelia Barrionuevo <noe.br97@gmail.com>
2025-12-14 00:12:48 +0100

    back. usuario service

---
+++ backend/database/UsuarioService.js
@@ -0,0 +1,15 @@
+import Usuario from "../models/Usuario";
+
+class UsuarioService {
+    async crearUsuario(data) {
+        return await Usuario.create(data);
+    }
+
+    async obtenerUsuarioPorEmail(email) {
+        return await Usuario.findOne({ where: { email } });
+    }
+
+    async obtenerUsuarioPorNick(nick) {
+        return await Usuario.findOne({ where: { nick } });
+    }
+}
