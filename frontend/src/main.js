4c5b80cdb02b8223c3c0ea6f4858cb1b3b081d45
Noelia Barrionuevo <noe.br97@gmail.com>
2025-12-13 18:55:28 +0100

    front. funcionalidad basica en el index

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,2 +1,28 @@
 import './style.css'
 
+document.addEventListener('DOMContentLoaded', () => {
+  const btnLogin = document.getElementById('btn-login');
+  const btnRegistro = document.getElementById('btn-registro');
+  const modal = document.getElementsByClassName('modal');
+  const modalLogin = document.getElementById('modal-login');
+  const modalRegistro = document.getElementById('modal-registro');
+  const closeButtons = document.querySelectorAll('.boton-cerrar');
+  const btnEnviarLogin = document.getElementById('submit-login');
+  const btnEnviarRegistro = document.getElementById('submit-registro');
+
+  btnLogin.addEventListener('click', () => {
+    modalLogin.style.display = 'flex';
+    modalRegistro.style.display = 'none';
+  });
+
+  btnRegistro.addEventListener('click', () => {
+    modalRegistro.style.display = 'flex';
+    modalLogin.style.display = 'none';
+  })
+
+  closeButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      btn.closest('.modal').style.display = 'none';
+    })
+  })
+})
